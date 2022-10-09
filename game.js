29a01265b2f8a54054ac9294ff813de2e3ccd108
Hattinger04 <s.8greiderer@gmail.com>
2022-10-09 15:33:40 +0200

    finishing work

--- game.js
+++ game.js
@@ -9,10 +9,15 @@ let game = {
         this.clear();
         this.interval = setInterval(redraw, 20);
         this.intervalNewEnemy = setInterval(newEnemy, 600);
+        this.intervalNewBonusstone = setInterval(newBonusstone, 2500);
+        this.intervalTime = setInterval(updateTime, 1000); 
+        this.time = 0;
+        this.score = 0;
         let image = new Image();
-        image.src = "img/face-cool.png"     
+        image.src = "img/face-cool.png"; 
         this.player = new sprite(30, 30, image, 10, 120);
         this.enemies = [];
+        this.bonusstones = []; 
         this.keyCode = -1; //when there is no key pressed
         window.addEventListener('keydown', (e) =>
         {
@@ -26,13 +31,19 @@ let game = {
     },
     clear() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
+    },
+    runAgain() {
+        clearInterval(this.interval); 
+        clearInterval(this.intervalNewEnemy); 
+        clearInterval(this.intervalNewBonusstone); 
+        clearInterval(this.intervalTime); 
+        game.start();
+    }
 }
 
 function start() {
     console.log("Game started");
     game.start();
-
 }
 
 
@@ -41,7 +52,7 @@ function sprite(width, height, image, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
-  this.image = image
+  this.image = image;
 
   ctx = game.context;
   ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
@@ -69,32 +80,86 @@ function redraw() {
    case 40: //down
        game.player.y += 1;
       break;
-}
+   }
+   if(game.player.x > game.canvas.width - 30) {
+        game.player.x = game.canvas.width - 30; 
+   } else if(game.player.x < 0) {
+        game.player.x = 0; 
+   }
+   if(game.player.y > game.canvas.height - 30) {
+        game.player.y = game.canvas.height - 30;
+   } else if(game.player.y < 0) {
+        game.player.y = 0; 
+   }
 
   game.player.redraw();
 
 
    game.enemies.forEach((e) => {
-       console.log(e)
        let yDelta = Math.floor(Math.random() * 11) - 5;
        e.x -= 1;
        e.y += yDelta;
        e.redraw();
     })
 
-    checkCollision()
+    game.bonusstones.forEach((e) => {
+        let yDelta = Math.floor(Math.random() * 11) - 5;
+        e.x -= 1;
+        e.y += yDelta;
+        e.redraw();
+    })
+
+    checkCollision();
+    drawScore();
+    drawTime(); 
 }
 
 function checkCollision() {
-    
+    distance = 15; 
+    game.bonusstones.forEach((e) => {
+        if(Math.abs(game.player.x - e.x) < distance && Math.abs(game.player.y - e.y) < distance) {
+            game.bonusstones.splice(game.bonusstones.indexOf(e), 1); 
+            game.score += 5; 
+        }
+    }); 
+    game.enemies.forEach((e) => {
+        if(Math.abs(game.player.x - e.x) < distance && Math.abs(game.player.y - e.y) < distance) {
+            game.runAgain(); 
+        }
+    }); 
 }
+
+function drawScore() {
+    ctx.font = "20px Arial";
+    ctx.strokeText("Score: " + game.score, 50, 50); 
+}
+
+function drawTime() {
+    ctx.font = "20px Arial";
+    ctx.strokeText("Time: " + game.time, 150, 50); 
+}
+
 
 function newEnemy()
 {
     let y = Math.floor(Math.random()*1024);
     let image = new Image();
-    image.src = "img/face-monkey.png" 
+    image.src = "img/face-devilish.png" 
     e = new sprite(30, 30, image, 1000, y);
     game.enemies.push(e);
 
+}
+
+function newBonusstone()
+{
+    let y = Math.floor(Math.random()*1024);
+    let image = new Image();
+    image.src = "img/face-monkey.png" 
+    e = new sprite(30, 30, image, 1000, y);
+    game.bonusstones.push(e);
+}
+
+function updateTime() {
+    game.time += 1;  
+    game.score += 1;  
 }
